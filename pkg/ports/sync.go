712d1922b42500f33ee1d4637d74bcc9f24a58a5
Mike Gregory <mike.gregory@ovo.com>
2022-10-03 15:40:41 +0100

    Simplify ports into a single file

--- pkg/ports/sync.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "context"
-
-// The Sync interface can be used for downstream services that implement Sync in your own workflow.
-type Sync interface {
-	SyncWith(ctx context.Context, adapter Adapter) error // Sync the things in a source service with this service.
-}
