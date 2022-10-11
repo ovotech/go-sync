11dfbeb92fbdd194e363bcc26d6c38ebb235587e
Mike Gregory <mike.gregory@ovo.com>
2022-10-11 15:50:42 +0100

    Add operating modes to Sync, replacing Add/Remove

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -13,6 +13,19 @@ import (
 	"github.com/ovotech/go-sync/pkg/ports"
 )
 
+type operatingMode string
+
+const (
+	// AddOnly only runs add operations.
+	AddOnly operatingMode = "Add"
+	// RemoveOnly only runs remove operations.
+	RemoveOnly operatingMode = "Remove"
+	// RemoveAdd first removes things, then adds them.
+	RemoveAdd operatingMode = "RemoveAdd"
+	// AddRemove first adds things, then removes them.
+	AddRemove operatingMode = "AddRemove"
+)
+
 // generateHashMap takes a list of strings and returns a hashed map of { item => true }.
 func generateHashMap(i []string) map[string]bool {
 	out := map[string]bool{}
@@ -24,23 +37,21 @@ func generateHashMap(i []string) map[string]bool {
 }
 
 type Sync struct {
-	DryRun bool            // DryRun mode calculates membership, but doesn't add or remove.
-	Add    bool            // Perform Add operations.
-	Remove bool            // Perform Remove operations.
-	source ports.Adapter   // The source adapter.
-	cache  map[string]bool // cache prevents polling the source more than once.
-	logger types.Logger
+	DryRun        bool            // DryRun mode calculates membership, but doesn't add or remove.
+	OperatingMode operatingMode   // Change the order of Sync's operation. Default is RemoveAdd.
+	source        ports.Adapter   // The source adapter.
+	cache         map[string]bool // cache prevents polling the source more than once.
+	logger        types.Logger
 }
 
 // New creates a new Sync service.
 func New(source ports.Adapter, optsFn ...func(*Sync)) *Sync {
 	sync := &Sync{
-		DryRun: false,
-		Add:    true,
-		Remove: true,
-		source: source,
-		cache:  make(map[string]bool),
-		logger: log.New(os.Stderr, "[go-sync/sync] ", log.LstdFlags|log.Lshortfile|log.Lmsgprefix),
+		DryRun:        false,
+		OperatingMode: RemoveAdd,
+		source:        source,
+		cache:         make(map[string]bool),
+		logger:        log.New(os.Stderr, "[go-sync/sync] ", log.LstdFlags|log.Lshortfile|log.Lmsgprefix),
 	}
 
 	for _, fn := range optsFn {
@@ -101,42 +112,38 @@ func WithLogger(logger types.Logger) func(*Sync) {
 	}
 }
 
-// remove processes removing things from a destination service.
-func (s *Sync) remove(ctx context.Context, things []string, removeFn func(context.Context, []string) error) error {
-	thingsToRemove := s.getThingsToRemove(things)
+// perform processes adding/removing things from a destination service.
+func (s *Sync) perform(
+	ctx context.Context,
+	action string,
+	things []string,
+	diffFn func(things []string) []string,
+	executeFn func(context.Context, []string) error,
+) func() error {
+	return func() error {
+		s.logger.Printf("Processing things to %s\n", action)
 
-	if s.DryRun || !s.Remove {
-		s.logger.Printf("Would remove %s, but remove is disabled", thingsToRemove)
+		thingsToChange := diffFn(things)
+
+		if s.DryRun {
+			s.logger.Printf("Would %s %s, but running in dry run mode", action, thingsToChange)
+
+			return nil
+		}
+
+		if len(thingsToChange) == 0 {
+			return nil
+		}
+
+		s.logger.Printf("%s: %s", action, thingsToChange)
+
+		err := executeFn(ctx, thingsToChange)
+		if err != nil {
+			return fmt.Errorf("%s(%v) -> %w", action, things, err)
+		}
 
 		return nil
 	}
-
-	if len(thingsToRemove) == 0 {
-		return nil
-	}
-
-	s.logger.Printf("Removing %s", thingsToRemove)
-
-	return removeFn(ctx, thingsToRemove)
-}
-
-// add processes adding things to a destination service.
-func (s *Sync) add(ctx context.Context, things []string, removeFn func(context.Context, []string) error) error {
-	thingsToAdd := s.getThingsToAdd(things)
-
-	if s.DryRun || !s.Add {
-		s.logger.Printf("Would add %s, but add is disabled", thingsToAdd)
-
-		return nil
-	}
-
-	if len(thingsToAdd) == 0 {
-		return nil
-	}
-
-	s.logger.Printf("Adding %s", thingsToAdd)
-
-	return removeFn(ctx, thingsToAdd)
 }
 
 // SyncWith synchronises the destination service with the source service, adding & removing things as necessary.
@@ -155,18 +162,34 @@ func (s *Sync) SyncWith(ctx context.Context, adapter ports.Adapter) error {
 		return fmt.Errorf("sync.syncwith.get -> %w", err)
 	}
 
-	s.logger.Println("Processing things to remove")
+	operations := make([]func() error, 0, 2) //nolint:gomnd
 
-	err = s.remove(ctx, things, adapter.Remove)
-	if err != nil {
-		return fmt.Errorf("sync.syncwith.remove -> %w", err)
+	switch s.OperatingMode {
+	case AddOnly:
+		operations = []func() error{
+			s.perform(ctx, "add", things, s.getThingsToAdd, adapter.Add),
+		}
+	case RemoveOnly:
+		operations = []func() error{
+			s.perform(ctx, "remove", things, s.getThingsToRemove, adapter.Remove),
+		}
+	case RemoveAdd:
+		operations = []func() error{
+			s.perform(ctx, "remove", things, s.getThingsToRemove, adapter.Remove),
+			s.perform(ctx, "add", things, s.getThingsToAdd, adapter.Add),
+		}
+	case AddRemove:
+		operations = []func() error{
+			s.perform(ctx, "add", things, s.getThingsToAdd, adapter.Add),
+			s.perform(ctx, "remove", things, s.getThingsToRemove, adapter.Remove),
+		}
 	}
 
-	s.logger.Println("Processing things to add")
-
-	err = s.add(ctx, things, adapter.Add)
-	if err != nil {
-		return fmt.Errorf("sync.syncwith.add -> %w", err)
+	for _, fn := range operations {
+		err = fn()
+		if err != nil {
+			return fmt.Errorf("sync.syncwith.execute -> %w", err)
+		}
 	}
 
 	s.logger.Println("Finished sync")
