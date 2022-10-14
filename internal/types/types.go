0a8b061da65619b1825587214ba934705024f251
Mike Gregory <mike.gregory@ovo.com>
2022-10-14 14:23:58 +0100

    Remove types, and replace with `*log.Logger`

--- internal/types/types.go
+++
@@ -1,7 +0,0 @@
-package types
-
-// Logger is a subset of log.Logger to allow compatible loggers to be used with Sync.
-type Logger interface {
-	Println(v ...any)
-	Printf(format string, v ...any)
-}
