9efb30a162d8690126b6dadeb17eaf5b0f2a0b96
Mike Gregory <mike.gregory@ovo.com>
2022-10-18 10:59:08 +0100

    Rename InitKeys to match string for easier debugging

--- adapters/github/team/team_internal_test.go
+++ adapters/github/team/team_internal_test.go
@@ -15,10 +15,10 @@ func TestNew(t *testing.T) {
 	t.Parallel()
 
 	discovery := NewMockGitHubDiscovery(t)
-	adapter := New(&github.Client{}, discovery, "Org", "Slug")
+	adapter := New(&github.Client{}, discovery, "GitHubOrg", "GitHubTeamSlug")
 
-	assert.Equal(t, "Org", adapter.org)
-	assert.Equal(t, "Slug", adapter.slug)
+	assert.Equal(t, "GitHubOrg", adapter.org)
+	assert.Equal(t, "GitHubTeamSlug", adapter.slug)
 }
 
 func TestTeam_Get(t *testing.T) {
@@ -28,18 +28,18 @@ func TestTeam_Get(t *testing.T) {
 
 	gitHubClient := newMockIGitHubTeam(t)
 	discovery := NewMockGitHubDiscovery(t)
-	adapter := New(&github.Client{}, discovery, "Org", "Slug")
+	adapter := New(&github.Client{}, discovery, "GitHubOrg", "GitHubTeamSlug")
 	adapter.teams = gitHubClient
 
 	gitHubClient.
 		EXPECT().
-		ListTeamMembersBySlug(ctx, "Org", "Slug", &github.TeamListTeamMembersOptions{}).
+		ListTeamMembersBySlug(ctx, "GitHubOrg", "GitHubTeamSlug", &github.TeamListTeamMembersOptions{}).
 		Return([]*github.User{
 			{Login: github.String("foo")},
 		}, &github.Response{NextPage: 1}, nil)
 	gitHubClient.
 		EXPECT().
-		ListTeamMembersBySlug(ctx, "Org", "Slug", &github.TeamListTeamMembersOptions{
+		ListTeamMembersBySlug(ctx, "GitHubOrg", "GitHubTeamSlug", &github.TeamListTeamMembersOptions{
 			ListOptions: github.ListOptions{Page: 1},
 		}).
 		Return([]*github.User{
@@ -62,15 +62,15 @@ func TestTeam_Add(t *testing.T) {
 
 	gitHubClient := newMockIGitHubTeam(t)
 	discovery := NewMockGitHubDiscovery(t)
-	adapter := New(&github.Client{}, discovery, "Org", "Slug")
+	adapter := New(&github.Client{}, discovery, "GitHubOrg", "GitHubTeamSlug")
 	adapter.teams = gitHubClient
 
 	discovery.EXPECT().GetUsernameFromEmail(ctx, []string{"fizz@email", "buzz@email"}).
 		Maybe().Return([]string{"fizz", "buzz"}, nil)
 	discovery.EXPECT().GetUsernameFromEmail(ctx, []string{"fizz@email", "buzz@email"}).
 		Maybe().Return([]string{"buzz", "fizz"}, nil)
-	gitHubClient.EXPECT().AddTeamMembershipBySlug(ctx, "Org", "Slug", "fizz", mock.Anything).Return(nil, nil, nil)
-	gitHubClient.EXPECT().AddTeamMembershipBySlug(ctx, "Org", "Slug", "buzz", mock.Anything).Return(nil, nil, nil)
+	gitHubClient.EXPECT().AddTeamMembershipBySlug(ctx, "GitHubOrg", "GitHubTeamSlug", "fizz", mock.Anything).Return(nil, nil, nil)
+	gitHubClient.EXPECT().AddTeamMembershipBySlug(ctx, "GitHubOrg", "GitHubTeamSlug", "buzz", mock.Anything).Return(nil, nil, nil)
 
 	err := adapter.Add(ctx, []string{"fizz@email", "buzz@email"})
 
@@ -84,12 +84,12 @@ func TestTeam_Remove(t *testing.T) {
 
 	gitHubClient := newMockIGitHubTeam(t)
 	discovery := NewMockGitHubDiscovery(t)
-	adapter := New(&github.Client{}, discovery, "Org", "Slug")
+	adapter := New(&github.Client{}, discovery, "GitHubOrg", "GitHubTeamSlug")
 	adapter.teams = gitHubClient
 	adapter.cache = map[string]string{"foo@email": "foo", "bar@email": "bar"}
 
-	gitHubClient.EXPECT().RemoveTeamMembershipBySlug(ctx, "Org", "Slug", "foo").Return(nil, nil)
-	gitHubClient.EXPECT().RemoveTeamMembershipBySlug(ctx, "Org", "Slug", "bar").Return(nil, nil)
+	gitHubClient.EXPECT().RemoveTeamMembershipBySlug(ctx, "GitHubOrg", "GitHubTeamSlug", "foo").Return(nil, nil)
+	gitHubClient.EXPECT().RemoveTeamMembershipBySlug(ctx, "GitHubOrg", "GitHubTeamSlug", "bar").Return(nil, nil)
 
 	err := adapter.Remove(ctx, []string{"foo@email", "bar@email"})
 
@@ -104,10 +104,10 @@ func TestInit(t *testing.T) {
 		t.Parallel()
 
 		adapter, err := Init(map[InitKey]string{
-			Token:     "token",
-			Org:       "org",
-			Slug:      "slug",
-			Discovery: "saml",
+			GitHubToken:     "token",
+			GitHubOrg:       "org",
+			GitHubTeamSlug:  "slug",
+			GitHubDiscovery: "saml",
 		})
 
 		assert.NoError(t, err)
@@ -124,52 +124,52 @@ func TestInit(t *testing.T) {
 			t.Parallel()
 
 			_, err := Init(map[InitKey]string{
-				Org:       "org",
-				Slug:      "slug",
-				Discovery: "saml",
+				GitHubOrg:       "org",
+				GitHubTeamSlug:  "slug",
+				GitHubDiscovery: "saml",
 			})
 
 			assert.ErrorIs(t, err, gosync.ErrMissingConfig)
-			assert.ErrorContains(t, err, Token)
+			assert.ErrorContains(t, err, GitHubToken)
 		})
 
 		t.Run("missing org", func(t *testing.T) {
 			t.Parallel()
 
 			_, err := Init(map[InitKey]string{
-				Token:     "token",
-				Slug:      "slug",
-				Discovery: "saml",
+				GitHubToken:     "token",
+				GitHubTeamSlug:  "slug",
+				GitHubDiscovery: "saml",
 			})
 
 			assert.ErrorIs(t, err, gosync.ErrMissingConfig)
-			assert.ErrorContains(t, err, Org)
+			assert.ErrorContains(t, err, GitHubOrg)
 		})
 
 		t.Run("missing slug", func(t *testing.T) {
 			t.Parallel()
 
 			_, err := Init(map[InitKey]string{
-				Token:     "token",
-				Org:       "org",
-				Discovery: "saml",
+				GitHubToken:     "token",
+				GitHubOrg:       "org",
+				GitHubDiscovery: "saml",
 			})
 
 			assert.ErrorIs(t, err, gosync.ErrMissingConfig)
-			assert.ErrorContains(t, err, Slug)
+			assert.ErrorContains(t, err, GitHubTeamSlug)
 		})
 
 		t.Run("missing discovery", func(t *testing.T) {
 			t.Parallel()
 
 			_, err := Init(map[InitKey]string{
-				Token: "token",
-				Org:   "org",
-				Slug:  "slug",
+				GitHubToken:    "token",
+				GitHubOrg:      "org",
+				GitHubTeamSlug: "slug",
 			})
 
 			assert.ErrorIs(t, err, gosync.ErrMissingConfig)
-			assert.ErrorContains(t, err, Discovery)
+			assert.ErrorContains(t, err, GitHubDiscovery)
 		})
 	})
 
@@ -177,10 +177,10 @@ func TestInit(t *testing.T) {
 		t.Parallel()
 
 		_, err := Init(map[InitKey]string{
-			Token:     "token",
-			Org:       "org",
-			Slug:      "slug",
-			Discovery: "foo",
+			GitHubToken:     "token",
+			GitHubOrg:       "org",
+			GitHubTeamSlug:  "slug",
+			GitHubDiscovery: "foo",
 		})
 
 		assert.ErrorIs(t, err, gosync.ErrInvalidConfig)
