79a4437a342d196f4d6a09d71bccd1d28844fffa
Mike Gregory <mike.gregory@ovo.com>
2022-10-18 09:24:22 +0100

    Add new Init config errors

--- errors.go
+++ errors.go
@@ -10,3 +10,9 @@ var ErrCacheEmpty = errors.New("cache is empty, run Get first")
 
 // ErrReadOnly is returned for adapters that cannot Add/Remove, but have been set as a destination.
 var ErrReadOnly = errors.New("cannot perform action, adapter is readonly")
+
+// ErrMissingConfig is returned when an InitFn is missing a required configuration.
+var ErrMissingConfig = errors.New("missing configuration")
+
+// ErrInvalidConfig is returned when an InitFn is passed an invalid configuration.
+var ErrInvalidConfig = errors.New("invalid configuration")
