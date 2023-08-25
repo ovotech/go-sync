3ed82b5e9dc354f6b6c3fc1c94bc20ed0fa3810d
Chris Brindley <chris.brindley@ovo.com>
2023-08-25 12:00:47 +0100

    Add tests

---
+++ adapters/azuread/user/user_integration_test.go
@@ -0,0 +1,28 @@
+//go:build integration
+
+package user
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	gosync "github.com/ovotech/go-sync"
+)
+
+var email = flag.String("email", "test@example.com", "Enter the email of a user to search for in the Integration test")
+
+func TestIntegration(t *testing.T) {
+	ctx := context.TODO()
+	adapter, err := Init(ctx, map[gosync.ConfigKey]string{
+		Filter: fmt.Sprintf("mail eq '%s'", *email),
+	})
+	assert.NoError(t, err)
+
+	emails, err := adapter.Get(ctx)
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, emails, []string{*email})
+}
