5a0d682b5c41489e132b42c71627d0f30c57d655
Mike Gregory <mike.gregory@ovo.com>
2023-08-25 15:19:44 +0100

    Update InitFn and ConfigFn to support Go's generics

--- types.go
+++ types.go
@@ -14,12 +14,12 @@ type Service interface {
 	SyncWith(ctx context.Context, adapter Adapter) error // Sync the things in a source service with this service.
 }
 
+// ConfigKey is a configuration key to Init a new adapter.
+type ConfigKey = string
+
 // A ConfigFn is used to pass additional or custom functionality to an adapter.
-type ConfigFn = func(interface{})
+type ConfigFn[T Adapter] func(T)
 
 // InitFn is an optional adapter function that can initialise a new adapter using a static configuration.
 // This is to make it easier to use an adapter in a CLI or other service that invokes adapters programmatically.
-type InitFn = func(ctx context.Context, config map[string]string, configFns ...ConfigFn) (Adapter, error)
-
-// ConfigKey is a configuration key to Init a new adapter.
-type ConfigKey = string
+type InitFn[T Adapter] func(context.Context, map[ConfigKey]string, ...ConfigFn[T]) (T, error)
