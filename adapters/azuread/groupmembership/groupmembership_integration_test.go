ce6bfa323d5f9c6730a584733187fb6d97b0e18a
Chris Brindley <chris.brindley@ovo.com>
2023-09-06 13:49:49 +0100

    Add an integration test

---
+++ adapters/azuread/groupmembership/groupmembership_integration_test.go
@@ -0,0 +1,51 @@
+//go:build integration
+
+package groupmembership
+
+import (
+	"context"
+	"flag"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	gosync "github.com/ovotech/go-sync"
+)
+
+var group = flag.String("group", "", "Enter the group to adjust group membership of in the Integration test")
+
+var email = flag.String("email", "", "Enter the email of a user to change membership of in the Integration test")
+
+func TestIntegration(t *testing.T) {
+	if *group == "" {
+		t.Fatalf("Required parameter 'group' is missing or empty.")
+	}
+
+	if *email == "" {
+		t.Fatalf("Required parameter 'email' is missing or empty.")
+	}
+
+	ctx := context.TODO()
+	adapter, err := Init(ctx, map[gosync.ConfigKey]string{
+		GroupName: *group,
+	})
+	assert.NoError(t, err)
+
+	emails, err := adapter.Get(ctx)
+	assert.NoError(t, err)
+	assert.NotContainsf(t, emails, *email, "Email %s already exists in the group %s", *email, *group)
+
+	err = adapter.Add(ctx, []string{*email})
+	assert.NoError(t, err)
+
+	emails, err = adapter.Get(ctx)
+	assert.NoError(t, err)
+	assert.Containsf(t, emails, *email, "Email %s not found after adding it to the group", *email)
+
+	err = adapter.Remove(ctx, []string{*email})
+	assert.NoError(t, err)
+
+	emails, err = adapter.Get(ctx)
+	assert.NoError(t, err)
+	assert.NotContainsf(t, emails, *email, "Email %s still exists after removing it from the group %s", *email, *group)
+}
